ff0756a220857c049e01da54afcc95fe56fae150
vr009 <slavarianov@yandex.ru>
2021-03-15 04:54:28 +0300

    yml works

--- ci/install-linux.sh
+++ ci/install-linux.sh
@@ -1,4 +1,4 @@
-GTEST_DIR=$(pwd)/gtest
+GTEST_DIR=~/gtest
 GTEST_URL=https://github.com/google/googletest/archive/release-1.10.0.tar.gz
 GTEST_TAR=gtest-release-1.10.0.tar.gz
 
@@ -6,7 +6,7 @@ wget $GTEST_URL
 mkdir -p $GTEST_DIR
 tar -xvf $GTEST_TAR $GTEST_DIR
 cd ${GTEST_DIR}
-cd googletest-release-1.10.0
+cd $GTEST_DIR/googletest-release-1.10.0
 mkdir build
 cd build
 cmake ..
