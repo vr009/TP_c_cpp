e36e7508bd8b3ad662ebab520e238153adf7e8d7
vr009 <slavarianov@yandex.ru>
2021-03-15 03:23:55 +0300

    try to write some yml file

---
+++ ci/install-linux.sh
@@ -0,0 +1,8 @@
+GTEST_DIR=$(pwd)/gtest
+GTEST_URL=https://github.com/google/googletest/archive/release-1.10.0.tar.gz
+GTEST_TAR=gtest-release-1.10.0.tar.gz
+
+wget --quit $GTEST_URL
+mkdir -p $GTEST_DIR
+tar -xzf $CMAKE_TAR -C $CMAKE_DIR
+export PATH=$GTEST_DIR/bin:$PATH
