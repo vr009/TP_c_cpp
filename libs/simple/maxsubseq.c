6d2bcfcfc3afeb2f569522b84ef41a826bf83413
vr009 <slavarianov@yandex.ru>
2021-03-26 02:21:34 +0300

    Static lib initialized with some interface

--- libs/simple/maxsubseq.c
+++ libs/simple/maxsubseq.c
@@ -1,5 +1,22 @@
 #include "maxsubseq.h"
 
+
+/*char *strpbrk(const char *s, const char *accept); - находит первое вхождение любого символа, перечисленного в accept*/
+
+
+struct substr_descriptor{
+    size_t index;
+    size_t substr_size;
+};
+
+typedef struct substr_descriptor substr_d;
+
+
+substr_d * get_des(char * input, size_t input_size, size_t start_i){
+
+}
+
+
 char * max_subseq(char * input, size_t size){
 
 }
