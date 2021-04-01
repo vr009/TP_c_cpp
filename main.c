8e38face47259c01dcb82c2b946e82a2f3aabbd5
vr009 <slavarianov@yandex.ru>
2021-04-01 14:06:17 +0300

    ability to add output file, stdout is default, everything passes

--- main.c
+++ main.c
@@ -22,10 +22,23 @@
 
 int main(int argc, char **argv){
 
+    //файл для вывода результата, по дефолту будем задавать stdout
+
     if(argc < 2){
-        printf("Usage <program> <input_data.txt>\n");
+        fprintf( stdout, "Usage <program> <input_data.txt> <output_file>\n");
         return 0;
     }
+
+    FILE * f_out = NULL;
+
+    if(argc == 3){
+        f_out = fopen(argv[2],  "w" );
+    }
+
+    if( f_out == NULL || argc == 2){
+        f_out = stdout;
+    }
+
 
     FILE *f = fopen(argv[1], "r");
 
@@ -36,7 +49,11 @@ int main(int argc, char **argv){
         char * input = mmap(NULL, file_size - 1, PROT_READ, MAP_SHARED | MAP_PRIVATE, fileno(f), 0);
 
         if ( input == NULL ) {
-            printf("Failed to map\n");
+
+            fprintf( f_out,"Failed to map\n");
+
+            fclose(f);
+            fclose(f_out);
             return 1;
         }
 
@@ -48,14 +65,14 @@ int main(int argc, char **argv){
         char * output = trigger(input, file_size - 1);
 
         if(output != NULL)
-            printf("Simple implementation's answer: %s\n", output);
+            fprintf( f_out, "Simple implementation's answer: %s\n", output);
         else
-            printf("Empty string\n");
+            fprintf( f_out, "Empty string\n");
 
         end_t = clock();
 
         double total_t = (double)(end_t - start_t) / CLOCKS_PER_SEC;
-        printf("(Simple version) Time taken: %lf\n", total_t);
+        fprintf( f_out, "(Simple version) Time taken: %lf\n", total_t);
 
         free(output);
 
@@ -68,7 +85,10 @@ int main(int argc, char **argv){
 
         if (NULL == library) {
             munmap(input, file_size);
-            printf("Lib ./libSUBSEQUENCE_STR_PARALLEL.so not found \n");
+            fprintf( f_out, "Lib ./libSUBSEQUENCE_STR_PARALLEL.so not found \n");
+
+            fclose(f);
+            fclose(f_out);
             return -1;
         }
 
@@ -81,23 +101,25 @@ int main(int argc, char **argv){
         output = mt_trigger(input, file_size);
 
         if(output != NULL)
-            printf("Parallel implementation's result: %s\n", output);
+            fprintf( f_out,"Parallel implementation's result: %s\n", output);
         else
-            printf("Empty string\n");
+            fprintf( f_out, "Empty string\n");
 
         end_t = clock();
 
         total_t = (double)(end_t - start_t) / CLOCKS_PER_SEC;
-        printf("(Parallel version) Time taken: %lf\n", total_t);
+        fprintf( f_out,"(Parallel version) Time taken: %lf\n", total_t);
 
         dlclose(library);
 
         free(output);
         munmap(input, file_size);
         fclose(f);
+        fclose(f_out);
 
     } else {
-        printf("File not found\n");
+        fprintf( stdout, "File not found\n");
+        fclose(f_out);
     }
 
     return 0;
